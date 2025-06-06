7698aff0d6f22f1ae336002186d16884e02180cd
Jayanaath <jayanaath.srirang004@gmail.com>
2025-06-06 18:52:24 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "bloo":"#00BCF1",
+        "blu":"#172B75",
+      }
+    },
+  },
+  plugins: [],
+}
+
